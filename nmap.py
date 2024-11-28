8dbce5cd147a6113bf7c02a9f7d7bfa6f3f8eaa1
Adeline <adelineeminian13@gmail.com>
2024-11-28 22:45:24 +0400

    error handling and messages are printing

--- nmap.py
+++ nmap.py
@@ -1,18 +1,46 @@
 import socket
 import sys
+from datetime import datetime
 
 if len(sys.argv) > 1:
+	now = datetime.now()
+	today = datetime.today()
+	name = socket.gethostname()
+	user_ip = socket.gethostbyname(socket.gethostname())
+
+	print("Starting Nmap RELQ at", today.date().isoformat() ,now.strftime("%H:%M"))
+	print("Nmap scan report for ", name, user_ip)
+
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	ip = sys.argv[1]
 	if len(sys.argv) > 2:
 		port = sys.argv[2]
-		result = server.connect_ex((ip, int(port)))
+		try:
+			result = server.connect_ex((ip, int(port)))
+		except OverflowError:
+			print("Error: port must be 0-65535")
+			server.close()
+			exit(1)
+		except ValueError:
+			print("Error: invalid literal for int() with base 10")
+			server.close()
+			exit(1)
 		if result == 0:
-			print(f"port {port} is open")
+			print("PORT", "  STATE", "SERVICE")
+			print("42/tcp", "open")
 	else:
 		port = 0
 		while port <= 1000:
-			result = server.connect_ex((ip, int(port)))
+			try:
+				result = server.connect_ex((ip, int(port)))
+			except OverflowError:
+				print("Error: port must be 0-65535")
+				server.close()
+				exit(1)
+			except ValueError:
+				print("Error: invalid literal for int() with base 10")
+				server.close()
+				exit(1)
 			if result == 0:
 				print(f"port {port} is open")
 			port = port + 1
